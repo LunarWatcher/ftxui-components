e93c1a0c70c7942161d6f8b8208afa6bd0de97c9
Olivia <zoe.i2k1@gmail.com>
2023-02-12 00:09:27 +0100

    Symlink compile-commands

--- src/ftxui-ext/components/Checkbox.hpp
+++ src/ftxui-ext/components/Checkbox.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/component.hpp"
+#include <ftxui/util/ref.hpp>
 
 namespace fxtui::ext::components {
 
 class CheckboxImpl : public ftxui::ComponentBase {
 public:
+    CheckboxImpl(ftxui::ConstStringRef label, bool* state, ftxui::Ref<ftxui::CheckboxOption> opt);
     
 };
 
